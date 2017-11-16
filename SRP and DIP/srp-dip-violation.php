01bbe226ad3e262bef992e82106944d2bc428e98
Sumit Pore <sumitpore@gmail.com>
2017-11-16 12:08:28 +0530

    Update srp-dip-violation.php

--- SRP and DIP/srp-dip-violation.php
+++ SRP and DIP/srp-dip-violation.php
@@ -6,7 +6,7 @@ class Reporter {
     //Create new DB connection
       $dbhandler = new MySQLDbConnect('localhost', 'root', 'root'); 
 
-      $users = $dbhandler->query('SELECT id, username, last_logged_in_at FROM users where last_logged_in_at >= 23/05/2016 00:00:00'); //getting list of all users
+      $users = $dbhandler->query('SELECT id, username FROM users where last_logged_in_at >= 23/05/2016 00:00:00'); //getting list of all users
 
       foreach($users as $singleUser){
           $message .= "Id: {$singleUser->id} \n";
